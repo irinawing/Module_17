17e669dbbb60ce8748e6125a9321925e82c1d8f3
IrinaWing <irina.wing@mail.ru>
2024-11-30 23:02:51 +0200

    Module 17_2 - Модели SQLALchemy. Отношения между таблицами.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from routers import task, user
+from app.routers import task, user
 
-app = FastAPI()
+app= FastAPI()
 
-@app.get("/")
+@app.get('/')
 async def welcome():
-    return {"message": "Welcome to Taskmanager"}
+    return {'message': 'Welcome to Taskmanager'}
 
 app.include_router(task.router)
 app.include_router(user.router)
